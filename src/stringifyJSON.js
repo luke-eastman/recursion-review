7eaa52e4bafa8db00fd36d4314ea660cca1f5104
Chance Nguyen <chanceknguyen@gmail.com>
2020-08-17 16:49:52 -0500

    Solve for stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+  var stringify = function(obj) {
+    if (obj === '') {
+      return;
+    } else if (typeof obj === 'number') {
+      result += obj;
+    } else if (typeof obj === 'boolean') {
+      result += obj;
+    } else if (obj === null) {
+      result += 'null';
+    } else if (typeof obj === 'string') {
+      result += '"' + obj + '"';
+    } else if (obj === undefined) {
+      return;
+    } else if (Array.isArray(obj)) {
+      result += '[';
+      for (var i = 0; i < obj.length; i++) {
+        stringify(obj[i]);
+        if (i < obj.length - 1) {
+          result += ',';
+        }
+      }
+      result += ']';
+    } else if (typeof obj === 'object') {
+      result += '{';
+      for (var key in obj) {
+        if (typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+          stringify(key);
+          result += ':';
+          stringify(obj[key]);
+          result += ',';
+        }
+      }
+      if (result[result.length - 1] === ',') {
+        result = result.slice(0, result.length - 1);
+      }
+      result += '}';
+    }
+  };
+  stringify(obj);
+  return result;
 };
